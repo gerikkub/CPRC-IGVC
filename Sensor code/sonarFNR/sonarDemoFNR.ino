009912b246c6aef58d3142c11adb95f1b95757dc
Joe <jleija42@gmail.com>
2013-11-13 17:29:17 -0800

    Update sonarDemoFNR.ino

--- Sensor code/sonarFNR/sonarDemoFNR.ino
+++ Sensor code/sonarFNR/sonarDemoFNR.ino
@@ -1,28 +1,31 @@
 #define trigPin0 13
 #define echoPin0 12
 #define MinDist 250
-
+//ss is the sure safe, i.e. how many times the sensor will recheck values before anouncing change
 int ss = 3;
 void setup() {
-  pinMode(trigPin0, OUTPUT);
+  pinMode(trigPin0, OUTPUT); //set up all the pins
   pinMode(echoPin0, INPUT);
   pinMode(3, OUTPUT);
   pinMode(2, OUTPUT);
   Serial.begin(9600);
 }
 
+/**
+ * Prevents light flickering between states unwarrented 
+ */
 void loop() 
 {
   
-  if (distance() < MinDist)
+  if (distance() < MinDist) //if something is within the distance
   {
-    for(int i = 0; i < ss; i++)
+    for(int i = 0; i < ss; i++) //DEBOUNCE
     {
       if (distance() >= MinDist)
       {
-        i = ss;
+        i = ss; //if something is ever outside of the distance, break out of the loop
       }
-      if (i == ss - 1)
+      if (i == ss - 1) //if you're super sure then make the change
       {
         digitalWrite(2,LOW);
         digitalWrite(3, HIGH);
@@ -32,7 +35,7 @@ void loop()
   }
   else
   {
-    for(int i = 0; i < ss; i++)
+    for(int i = 0; i < ss; i++) //identical to the above just for switching back
     {
       if (distance() < MinDist)
       {
