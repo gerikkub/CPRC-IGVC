470b80320f3d89c1d7039804d27295bd7bcefc06
Joe <jleija42@gmail.com>
2013-11-13 17:27:03 -0800

    Update sonarFNR.ino

--- Sensor code/sonarFNR/sonarFNR.ino
+++ Sensor code/sonarFNR/sonarFNR.ino
@@ -1,21 +1,9 @@
-/*
- HC-SR04 Ping distance sensor]
- VCC to arduino 5v GND to arduino GND
- Echo to Arduino pin 13 Trig to Arduino pin 12
- Red POS to Arduino pin 11
- Green POS to Arduino pin 10
- 560 ohm resistor to both LED NEG and GRD power rail
- More info at: http://goo.gl/kJ8Gl
- Original code improvements to the Ping sketch sourced from Trollmaker.com
- Some code and wiring inspired by http://en.wikiversity.org/wiki/User:Dstaub/robotcar
- */
-
-#define trigPin0 13
-#define echoPin0 12
-#define MinDist 250
+#define trigPin0 13 //put trig on 13
+#define echoPin0 12 //echo on 12
+#define MinDist 250 //maximum distance is 250 cm
 void setup() {
   Serial.begin (9600);
-  pinMode(trigPin0, OUTPUT);
+  pinMode(trigPin0, OUTPUT); //set up all the pins
   pinMode(echoPin0, INPUT);
   pinMode(3, OUTPUT);
   pinMode(10, OUTPUT);
@@ -25,20 +13,20 @@ void loop()
 {
   long duration0, duration1, distance0, distance1;
   digitalWrite(trigPin0, LOW);  
-  digitalWrite(trigPin0, HIGH);
+  digitalWrite(trigPin0, HIGH); //PING
   digitalWrite(trigPin0, LOW); 
-  duration0 = pulseIn(echoPin0, HIGH); 
+  duration0 = pulseIn(echoPin0, HIGH); //translate to a duration 
   delay(10);
-  distance0 = (duration0 / 2) / 29.1;
-  int brightness0 = 255 - distance0;
-  if (distance0 < MinDist)
+  distance0 = (duration0 / 2) / 29.1; //convert to CM, yes that is a magic number
+  int brightness0 = 255 - distance0; //this is old but worth keeping if LEDs will be used again
+  if (distance0 < MinDist) //if something is within distance
   {
     digitalWrite(2, LOW);
-    digitalWrite(3, HIGH);
+    digitalWrite(3, HIGH); //light up 3
   }
   else
   {
-    digitalWrite(3, LOW);
+    digitalWrite(3, LOW); //else light up 2
     digitalWrite(2, HIGH);
   }
   Serial.print("R:");
