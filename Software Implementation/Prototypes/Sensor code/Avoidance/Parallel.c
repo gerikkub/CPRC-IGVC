f8f59f7732960f366fb3c7d4335baf7162ec4bd4
jlin21 <jordanlin94@yahoo.com>
2013-11-23 15:15:07 -0800

    Moved folders into Software Implementation

---
+++ Software Implementation/Prototypes/Sensor code/Avoidance/Parallel.c
@@ -0,0 +1,13 @@
+// Sloppy psuedo code
+// Make cart parallel to left wall
+// Assuming it's far enough from the wall
+// that the front bumper won't bump into the wall
+int main (void) {
+
+	//if side sensors aren't too close to wall
+	//nose near wall = 90 - degree
+	//back near wall = 90 + degree
+	SideAngleToWall = angle
+
+	SetSteeringAngle(SideAngleToWall)
+}
