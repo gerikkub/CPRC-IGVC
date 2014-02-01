974ee22a47ac1b8e9f541d990d3c623dff65e2f5
Louie Thiros <lsthiros@gmail.com>
2014-02-01 15:24:53 -0800

    Restructure command parsing

--- Code/Caddy Board/Communication Protocol/protocol.h
+++ Code/Caddy Board/Communication Protocol/protocol.h
@@ -34,195 +34,76 @@ typedef struct response Response;
 //Ultrasonic group
 
 //distance in cm stored as int in sensorResponse, returns success
-char getCertainSensor(char sensor, int* sensorResponse) {
-   //returns dummy value for now
-   *sensorResponse = 0;
-   //return success
-   return 1;
-}
+char getCertainSensor(char sensor, int* sensorResponse); 
 
 //takes in 6 int array to store response, returns success
-char getAllSensors(int* sensorResponse) {
-   for(char i = 0; i < 6; i++) {
-      getCertainSensor(i, &sensorResponse[i]);
-   }
-   //no error checking for now, return success
-   return 1;
-}
+char getAllSensors(int* sensorResponse);
 
 //stores cm values of a sensor group in sensorResponse
-char getSensorGroup(char groupID, int *sensorResponse) {
-   //different group ID's might mean different lengths. the dummy
-   //implementation will place 3 0x0000's in the response
-   for(int i = 0; i < 3; i++) {
-      *sensorResponse=0;
-   }
-   //return success
-   return 1;
-}
+char getSensorGroup(char groupID, int *sensorResponse);
 
 //Speed Sensor group
 
 //get the speed of the vehicle
-char getSpeed(char* sensorResponse) {
-   //store dummy speed value
-   *sensorResponse = 0;
-   //return success
-   return 1;
-}
+char getSpeed(char* sensorResponse);
 
 //will set just the speed of the wheels of the car
-char setSpeed(char speedTarget) {
-   //nothing done down here
-   //return success
-   return 1;
-}
+char setSpeed(char speedTarget);
 
 //Steering group
 
 //JUST SETS ANGLE TARGET. Actual angle changing is lower level
-char setAngle(char angleTarget) {
-   //dummy function, nothing will happen for now...
-   //return success
-   return 1;
-}
+char setAngle(char angleTarget);
 
 //get the current angle of the steering system
-char getAngle(char* sensorResponse) {
-   //store fake angle reading
-   *sensorResponse = 0;
-   //return success
-   return 1;
-}
+char getAngle(char* sensorResponse);
 
 //get the angle target. argument name is sensorResponse for the sake of consistancy
-char getDesiredAngle(char* sensorResponse) {
-   //fake response
-   *sensorResponse = 0;
-   //return success
-   return 1;
-}
+char getDesiredAngle(char* sensorResponse);
 
 //change the PID controller
-char changePID(char P, char I, char D) {
-   //dummy function, nothing happens
-   //return success
-   return 1;
-}
+char changePID(char P, char I, char D);
 
 //not sure what this does. Also not sure why upper and lower are separated
 //but its in the specs so...
-char setLimits(char upper, char lower) {
-   //dummy function, nothing happens
-   //return success
-   return 1;
-}
+char setLimits(char upper, char lower);
 
 //FNR controller functions
 
 //set the FNR controller, inputs are either -1, 0, or 1.
-char setFNR(char FNR) {
-   //dummy function, nothing happens
-   //return success
-   return 1;
-}
+char setFNR(char FNR);
 
 //get the current FNR state. Function follows same format for consistancy
-char getFNR(char *sensorResponse) {
-   //fake reading
-   *sensorResponse = 0;
-   //return success
-   return 1;
-}
+char getFNR(char *sensorResponse);
 
 //Brake controller functions. THERE IS CURRENTLY NO WAY TO GET THE DESIRED
 //BRAKE TARGET IN THE SPECS
 
 //set brake target
-char setBrake(char gainTarget) {
-   //dummy function, nothing happens
-   //return success
-   return 1;
-}
+char setBrake(char gainTarget);
 
 //get what is assumed to be the current brake value?
-char getBrake(char* sensorResponse) {
-   //fake brake reading
-   *sensorResponse = 0;
-   //return success
-   return 1;
-}
+char getBrake(char* sensorResponse);
 
 //battery controller functions
 
 //read battery strength in a char. Might need to be unsigned, specs don't say
 //anything regarding that.
-char getBatteryVoltage(char *sensorResponse) {
-   //fake battery reading
-   *sensorResponse = 0;
-   //return success
-   return 1;
-}
+char getBatteryVoltage(char *sensorResponse);
 
 //gets voltage from steering?
-char getSteeringVoltage(char *sensorResponse) {
-   //fake steering voltage reading
-   *sensorResponse = 0;
-   //return success
-   return 1;
-}
+char getSteeringVoltage(char *sensorResponse);
 
 //light controller functions
 
 //turn lights on and off
-char setLight(char lightTarget) {
-   //dummy function, nothing happens
-   //return success
-   return 1;
-}
+char setLight(char lightTarget);
 
 //specs make no mention of a getLight
 
 //function to CRC the command structure
-char commandIntegCheck(Command *command) {
-   //CRC the command with 0x00 in place of the CRC
-   //return success for now
-   return 1;
-}
+char commandIntegCheck(Command *command);
 
 //Function that will take in a command (as char array) and process it into a
 //correct response to be stored into response
-char processCommand(Command *command, Response *response) {
-   if(commandIntegCheck(command)) {
-      switch(command->groupID) {
-         case ULTRASONIC_GROUP:
-            /*do ultrasonic things*/
-            break;
-         case SPEED_GROUP:
-            /*do speed things*/
-            break;
-         case STEERING_GROUP:
-            /*do steering things*/
-            break;
-         case FNR_GROUP:
-            /*do FRN things*/
-            break;
-         case BRAKES_GROUP:
-            /*do brakes things*/
-            break;
-         case BATTERY_GROUP:
-            /*do battery things*/
-            break;
-         case LIGHTS_GROUP:
-            /*do light things*/
-            break;
-         case ERROR_GROUP:
-            /*do error things*/
-            break;
-      }
-      //return a dummy success for now
-      return 1;
-   } else {
-      return 0;
-   }
-}
+char processCommand(Command *command, Response *response);
